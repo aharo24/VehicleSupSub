fb4fcb29f40dc88165983d249a1cc1a3ba750ae6
ItachiCodes <52618896+ItachiCodes@users.noreply.github.com>
2021-11-05 18:00:18 -0700

    Initial commit

---
+++ src/Vehicle.java
@@ -0,0 +1,11 @@
+public class Vehicle
+{
+    public final String make;
+    public final double cost;
+
+    Vehicle(String make, double cost){
+        this.make = make;
+        this.cost = cost;
+    }
+
+}
